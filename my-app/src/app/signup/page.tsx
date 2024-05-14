e8aa5a81794f80094d644da115b55aff0eb6a6c3
Fernando <fernandolovato227@gmail.com>
2024-05-14 15:50:03 -0300

    tela de cadastro

---
+++ my-app/src/app/signup/page.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image"
+import IMG from "../../../public/image 1.png"
+import style from "./style.module.css"
+
+
+export default function Signup() {
+    return(
+        <div className="telainteira flex flex-row">
+            <div className="form-image">
+                <Image src={IMG} width={400} height={400} className="w-[50vw] h-[100vh]"  />
+            </div>
+
+            <div className="form flex flex-col items-center w-[50vw] h-[100vh] justify-evenly bg-[#EDEDED]">
+                <h1 className="titulo text-3xl font-bold">Cadastro usuário</h1>
+                <div className="inputs flex flex-col w-full items-center h-[75%] justify-evenly">
+                <input type="text" className={style.input}  placeholder="Nome"/>
+                
+                <input type="email" className={style.input}  placeholder="Email"/>
+              
+                <input type="password" className={style.input}  placeholder="Senha"/>
+                
+                <input type="text" className={style.input}  placeholder="Curso"/>
+                
+                <input type="text" className={style.input}  placeholder="Departamento"/>
+                
+                <button className={style.botao}>Criar conta</button>
+                </div>
+            </div>
+            
+        </div>        
+    )
+}
