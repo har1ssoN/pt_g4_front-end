6f62e5a37f820405bed0849180a32dd507e42502
har1ssoN <harissonmagalhaes@gmail.com>
2024-06-12 23:03:54 -0300

    :construction: Botões da página deslogado estão com rotas corretas e nas publicações falta apenas os comentários

--- my-app/src/componentes/header deslogado.tsx
+++ my-app/src/componentes/header deslogado.tsx
@@ -1,12 +1,21 @@
+'use client';
 import React from 'react'
+import { useRouter } from "next/navigation";
 
 const CabecalhoDeslogado = () => {
+  const router = useRouter();
+
+  const handleLoginClick = () => {
+    router.push('/login'); // Redireciona para a página de login
+  };
   return (
     <header className='bg-[#a4fed3] flex p-4 justify-between'>
         <div className=''>
           <img src='/image 2.svg' alt=''/>
         </div>
-        <div className='bg-[#00abed] text-white rounded-full w-36 h-12 text-center text-2xl  justify-center mx-4 content-center p-3 border-white border-2 cursor-pointer shadow-xl'>
+        <div className='bg-[#00abed] text-white rounded-full w-36 h-12 text-center text-2xl  justify-center mx-4 content-center p-3 border-white border-2 cursor-pointer shadow-xl'
+        onClick={handleLoginClick} // Adiciona o redirecionamento ao clicar
+        >
           <button>
             Login
           </button>
